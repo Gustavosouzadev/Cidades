5f0d570146a31663ad5ca25a129a9e8b768020f7
Gustavo <Gustavo>
2021-02-26 22:47:49 -0300

    Finalizando o programa Cidades

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\----------------------\\");
+            Console.WriteLine("|Cidades do Litoral Sul|");
+            Console.WriteLine("\\----------------------\\");
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Peruíbe, \"No rio dos tubarões\"");
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Itanhaém, \"Pedra que Canta\"");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Mongaguá, \"Aguá Pegajosa\"");
+            Console.ResetColor();
+        }
+    }
+}
